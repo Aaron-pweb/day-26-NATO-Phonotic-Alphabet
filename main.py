103038afeafe6f78e33746f9bb6dea4088b1cfa2
Aaron Teshale <aaronteshale138@gmail.com>
2025-07-30 10:13:56 +0300

    Create main.py
    NATO Phonetic Alphabet Converter – 100 Days of Code (Day 26)

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+new_dictionary = {value["letter"]:value['code'] for (key,value) in df.iterrows()}
+print(new_dictionary)
+#using recursion
+def recursion_function():
+    user_input = input("enter a word: ").upper()
+    try:
+        coded_names = [new_dictionary[code] for code in user_input]
+    except KeyError:
+        print("only letters in the alphabet please! ")
+        recursion_function()
+    else:
+        print(coded_names)
+        recursion_function()
+recursion_function()
